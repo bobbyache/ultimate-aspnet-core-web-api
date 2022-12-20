e76e597631b8e3094b258305de37471cfd215a10
RobBl8ke <RobB@korbicom.com>
2022-12-20 18:42:17 +0200

    Adding the repository manager (attempt 2)

--- CompanyEmployees/Repository/RepositoryManager.cs
+++ CompanyEmployees/Repository/RepositoryManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    internal class RepositoryManager
+    public sealed class RepositoryManager : IRepositoryManager
     {
+        private readonly RepositoryContext repositoryContext;
+        private readonly Lazy<ICompanyRepository> companyRepository;
+        private readonly Lazy<IEmployeeRepository> employeeRepository;
+
+        public RepositoryManager(RepositoryContext repositoryContext)
+        {
+            this.repositoryContext = repositoryContext;
+            this.companyRepository = new Lazy<ICompanyRepository>(() => new CompanyRepository(repositoryContext));
+            this.employeeRepository = new Lazy<IEmployeeRepository>(() => new EmployeeRepository(repositoryContext));
+        }
+
+        public ICompanyRepository Company => companyRepository.Value;
+
+        public IEmployeeRepository Employee => employeeRepository.Value;
+
+        public void Save() => repositoryContext.SaveChanges();
     }
 }
